5e30373ee00794f31a51de4bc90c27e2b5ab7c2a
Josh Kraker <joshkrak@gmail.com>
2021-07-18 22:08:23 -0400

    Default script, saving tmp-script, started on utilities d.ts

--- web/src/components/Editor.vue
+++ web/src/components/Editor.vue
@@ -13,18 +13,73 @@
       },
       setup() {
 
+         const tmpScript = localStorage.getItem('tmp-script') ?? [
+            '"use strict";',
+            '',
+            'export class MyLedAnimation {',
+            '',
+            '  static meta = {',
+            '     params: [',
+            '     ]',
+            '  }',
+            '',
+            '  constructor() {',
+            '     this.#frame = [];',
+            '  }',
+            '',
+            '  setNumLeds(numLeds) {',
+            '     this.#frame = [];',
+            '     for(let i = 0; i < numLeds; i++) {',
+            '        this.#frame.push([0, 0, 0]);',
+            '     }',
+            '  }',
+            '',
+            '  setConfig(config) {',
+            '  }',
+            '',
+            '  nextFrame() {',
+            '     return this.#frame;',
+            '  }',
+            '',
+            '}',
+            ''
+         ].join('\r\n');
+
          onMounted(() => {
+
+            const utilities = [
+               'declare const utilities: {',
+               '  color: {',
+               '     /**', ,
+               '      * Converts HSL values to a [R, G, B] value',
+               '        @param h - Hue as a number between 0-360',
+               '        @param s - Saturation as a number between 0-100',
+               '        @param l - Luminance as a number between 0-100',
+               '      */',
+               '     hslToRgb(h: number, s: number, l: number): [number, number, number]',
+               '  }',
+               '}'
+            ].join('\r\n');
+            const utilitiesUrl = 'ts:filename/utilities.d.ts';
 
             (window as any).require(['vs/editor/editor.main'], function () {
 
                const thisMonaco: typeof monaco = (window as any).monaco;
 
+               thisMonaco.languages.typescript.javascriptDefaults.addExtraLib(utilities, utilitiesUrl);
+               thisMonaco.editor.createModel(utilities, 'typescript', thisMonaco.Uri.parse(utilitiesUrl));
+
                const editor = thisMonaco.editor.create(document.getElementById('editor-ide-container'), {
-                  value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
+                  value: tmpScript,
                   language: 'javascript',
                   renderValidationDecorations: 'on',
                   theme: 'vs-dark'
                }) as monaco.editor.IStandaloneCodeEditor
+
+               editor.onDidChangeModelContent(e => {
+                  const script = editor.getValue();
+                  localStorage.setItem('tmp-script', script);
+               });
             })
          })
 
