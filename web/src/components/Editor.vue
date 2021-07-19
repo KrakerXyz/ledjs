d24b4d047513ca3ab28e3861bb66baba81187508
Josh Kraker <joshkrak@gmail.com>
2021-07-18 20:57:45 -0400

    Loading from cdn

--- web/src/components/Editor.vue
+++ web/src/components/Editor.vue
@@ -1,39 +1,12 @@
 
 <template>
-   <div id="editor-ide-container" class="h-100"></div>
+   <div id="editor-ide-container" class="h-100 border"></div>
 </template>
 
 <script lang="ts">
 
    import { defineComponent, onMounted } from 'vue';
-
-   import * as monaco from 'monaco-editor'
-
-   //Ignoring errors due to ?worker. Still seems to work ok.
-   //IF we get errors in prod about window being undefined, see https://stackoverflow.com/questions/65953675/import-monaco-editor-using-vite-2
-   import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
-   import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
-   import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
-   import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
-   import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
-
-   (self as any).MonacoEnvironment = {
-      getWorker(_, label) {
-         if (label === 'json') {
-            return new jsonWorker()
-         }
-         if (label === 'css' || label === 'scss' || label === 'less') {
-            return new cssWorker()
-         }
-         if (label === 'html' || label === 'handlebars' || label === 'razor') {
-            return new htmlWorker()
-         }
-         if (label === 'typescript' || label === 'javascript') {
-            return new tsWorker()
-         }
-         return new editorWorker()
-      }
-   }
+   import type * as monaco from 'monaco-editor';
 
    export default defineComponent({
       props: {
@@ -41,16 +14,19 @@
       setup() {
 
          onMounted(() => {
-            try {
-               const editor = monaco.editor.create(document.getElementById('editor-ide-container'), {
-                  value: "function hello() {\n\talert('Hello world!');\n}",
-                  language: 'javascript'
-               });
 
-            } catch (e) {
-               document.getElementById('editor-ide-container').innerText = 'Error starting manaco';
-            }
-         });
+            (window as any).require(['vs/editor/editor.main'], function () {
+
+               const thisMonaco: typeof monaco = (window as any).monaco;
+
+               const editor = thisMonaco.editor.create(document.getElementById('editor-ide-container'), {
+                  value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
+                  language: 'javascript',
+                  renderValidationDecorations: 'on',
+                  theme: 'vs-dark'
+               }) as monaco.editor.IStandaloneCodeEditor
+            })
+         })
 
          return {};
       }
