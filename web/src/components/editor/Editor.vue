dd938caebf1d699a6cb796b3b24d803f79bab230
Josh Kraker <joshkrak@gmail.com>
2021-07-19 21:32:49 -0400

    iframe poc testing

--- web/src/components/editor/Editor.vue
+++ web/src/components/editor/Editor.vue
@@ -2,8 +2,11 @@
 <template>
    <div class="d-flex flex-column h-100">
       <div>
-         <button class="btn btn-link p-0" @click="resetScript()">
+         <button class="btn btn-link p-0 me-3" @click="resetScript()">
             Reset Script
+         </button>
+         <button class="btn btn-link p-0" @click="testScript()">
+            Test script
          </button>
       </div>
       <div id="editor-ide-container" class="flex-grow-1"></div>
@@ -48,7 +51,31 @@
             content.value = useDefaultScript();
          }
 
-         return { resetScript };
+         const testScript = () => {
+            if (!content?.value?.trim()) { return; }
+
+            //We'll need to create a postMessage here because to call the fn directly, we need to add allow-same-origin to sandbox but this also allows the script to access parent frame
+
+            const srcdoc = `<html><body><script>window.fn = () => { console.log('Inside iframe fn'); }<\/script><\/body><\/html>`;
+
+            (window as any).myfn = () => console.log('bad!');
+
+            const iframe = document.createElement('iframe');
+            iframe.srcdoc = srcdoc;
+            iframe.sandbox.add('allow-scripts');
+            iframe.style.display = 'none';
+
+            document.body.appendChild(iframe);
+
+            iframe.onload = () => {
+               console.log('iframe loaded');
+               (iframe.contentWindow as any).fn();
+               iframe.remove();
+            }
+
+         }
+
+         return { resetScript, testScript };
       }
    });
 
