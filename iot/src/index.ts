81c2ec7690e99e911b3c5605eeba093ac74fa0dd
Josh Kraker <joshkrak@gmail.com>
2021-07-31 15:17:47 -0400

    iot auto reconnect

--- iot/src/index.ts
+++ iot/src/index.ts
@@ -20,37 +20,54 @@ if (!getConfig(EnvKey.DeviceId) || !getConfig(EnvKey.DeviceSecret)) {
     console.log('Initializing leds');
     const leds = new Leds();
 
-    console.log(`Starting WebSocket @ ${remoteAddress}`);
-    const authToken = `${getRequiredConfig(EnvKey.DeviceId)}:${getRequiredConfig(EnvKey.DeviceSecret)}`;
-    const ws = new WebSocket(`ws://${remoteAddress}/ws/device`, { auth: authToken });
+    let reconnectRetryCount = 0;
+    const startWebsocket = () => {
 
-    ws.addEventListener('open', () => {
-        console.log('WebSocket opened');
-    });
+        console.log(`Starting WebSocket @ ${remoteAddress}`);
+        const authToken = `${getRequiredConfig(EnvKey.DeviceId)}:${getRequiredConfig(EnvKey.DeviceSecret)}`;
+        const ws = new WebSocket(`ws://${remoteAddress}/ws/device`, { auth: authToken });
 
-    ws.addEventListener('error', e => {
-        //Will happen if the server is down. Will subsequently call the close as well
-        console.warn(`WebSocket error: ${e.message}`);
-    });
+        ws.addEventListener('open', () => {
+            console.log('WebSocket opened');
 
-    ws.addEventListener('close', e => {
-        //Will happen if the server closes or after an error has occurred while connecting
-        console.warn('WebSocket closed', { wasClean: e.wasClean, code: e.code, reason: e.reason });
-    });
+            reconnectRetryCount = 0;
+        });
 
-    ws.addEventListener('message', e => {
-        try {
-            const message = JSON.parse(e.data) as ToDeviceMessage;
-            console.log(`Incoming WebSocket message: ${message.type}`);
-            switch (message.type) {
-                case 'ledSetup': {
-                    leds.setup(message.data);
-                    break;
-                }
+        ws.addEventListener('error', e => {
+            //Will happen if the server is down. Will subsequently call the close as well
+            console.warn(`WebSocket error: ${e.message}`);
+        });
+        ws.addEventListener('close', e => {
+            //Will happen if the server closes or after an error has occurred while connecting
+            if (e.code === 4001) {
+                console.error('Device not authorized to connect to netled. Check your .env DEVICE_ID/SECRET');
+                return;
             }
-        } catch (e) {
-            console.error(`Error parsing incoming WebSocket message - ${e}`);
-        }
-    });
+
+            reconnectRetryCount++;
+            const retryWaitSecs = Math.min(reconnectRetryCount, 15);
+            console.warn(`WebSocket closed. Attempting reconnect #${reconnectRetryCount} in ${retryWaitSecs}secs`, { wasClean: e.wasClean, code: e.code, reason: e.reason });
+            setTimeout(() => {
+                startWebsocket();
+            }, retryWaitSecs * 1000);
+        });
+
+        ws.addEventListener('message', e => {
+            try {
+                const message = JSON.parse(e.data) as ToDeviceMessage;
+                console.log(`Incoming WebSocket message: ${message.type}`);
+                switch (message.type) {
+                    case 'ledSetup': {
+                        leds.setup(message.data);
+                        break;
+                    }
+                }
+            } catch (e) {
+                console.error(`Error parsing incoming WebSocket message - ${e}`);
+            }
+        });
+
+    };
+    startWebsocket();
 
 })();
