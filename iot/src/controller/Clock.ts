b7e0ec9e1fbbcf2c8452003c4ba6ad663317df32
Josh Kraker <joshkrak@gmail.com>
2021-08-18 20:19:07 -0400

    Test

--- iot/src/controller/Clock.ts
+++ iot/src/controller/Clock.ts
@@ -19,14 +19,12 @@ export class Clock {
 
     private _isStopped: boolean = false;
     private _interval: number = Infinity;
-    private _intervalTimeout: NodeJS.Timeout | null = null;
     private setInterval(interval: number) {
         if (interval === this._interval) { return; }
         console.log(`Updated interval to ${interval}ms`);
         this._interval = interval;
-        if (this._intervalTimeout) { clearInterval(this._intervalTimeout); }
         if (this._isStopped) { return; }
-        this._intervalTimeout = setInterval(() => this.tick(), 3);
+        this.tick();
     }
 
     private setStopped(stopped: boolean) {
@@ -34,27 +32,46 @@ export class Clock {
         this._isStopped = stopped;
         if (stopped) {
             console.log('Animator clock stopped');
-            if (this._intervalTimeout) {
-                clearInterval(this._intervalTimeout);
-                this._intervalTimeout = null;
-            }
-        } else if (!this._intervalTimeout) {
+        } else {
             console.log('Animator clock started');
             //Change the next to now otherwise it'll play catch up and render all frames that otherwise would have been due during the pause period.
             this._nextDue = performance.now();
-            this._intervalTimeout = setInterval(() => this.tick(), 3);
+            this.tick();
         }
     }
+
+    private _clockSkew = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    private _skewPosition = 0;
 
     private _nextDue = performance.now();
     private tick() {
         if (this._isStopped) { return; }
+
         const now = performance.now();
         const offBy = now - this._nextDue;
-        if (offBy < -2) { return; }
-        //console.log(`Late: ${offBy}`);
-        this._nextDue = now + this._interval - offBy;
-        this._onTick();
+
+        if (offBy > 0) {
+            this._nextDue = now + this._interval - offBy;
+            this._onTick();
+
+            this._clockSkew[this._skewPosition] = Math.abs(offBy);
+            this._skewPosition++;
+            if (this._clockSkew.length === this._skewPosition) {
+                const avg = this._clockSkew.reduce((s, c) => s + c, 0) / this._clockSkew.length;
+                console.log(`Avg Skew: ${avg}`);
+                this._skewPosition = 0;
+            }
+        }
+
+        const dueIn = this._nextDue - now;
+        if (dueIn < 10) {
+            setImmediate(() => this.tick());
+        } else if (dueIn < 50) {
+            setTimeout(() => this.tick(), 10);
+        } else {
+            setTimeout(() => this.tick(), 30);
+        }
+
     }
 
 
