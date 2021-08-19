9bdb0c1dc268c38392d34128ef04c24854bf0e98
Josh Kraker <joshkrak@gmail.com>
2021-08-18 20:30:57 -0400

    Ticnum

--- iot/src/controller/Clock.ts
+++ iot/src/controller/Clock.ts
@@ -19,12 +19,14 @@ export class Clock {
 
     private _isStopped: boolean = false;
     private _interval: number = Infinity;
+    private _tickNum: number = 0;
     private setInterval(interval: number) {
         if (interval === this._interval) { return; }
         console.log(`Updated interval to ${interval}ms`);
         this._interval = interval;
         if (this._isStopped) { return; }
-        this.tick();
+        this._tickNum++;
+        this.tick(this._tickNum);
     }
 
     private setStopped(stopped: boolean) {
@@ -36,15 +38,14 @@ export class Clock {
             console.log('Animator clock started');
             //Change the next to now otherwise it'll play catch up and render all frames that otherwise would have been due during the pause period.
             this._nextDue = performance.now();
-            this.tick();
+            this._tickNum++;
+            this.tick(this._tickNum);
         }
     }
 
-    private _clockSkew = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    private _skewPosition = 0;
-
     private _nextDue = performance.now();
-    private tick() {
+    private tick(tickNum: number) {
+        if (tickNum !== this._tickNum) { return; }
         if (this._isStopped) { return; }
 
         const now = performance.now();
@@ -53,23 +54,13 @@ export class Clock {
         if (offBy > 0) {
             this._nextDue = now + this._interval - offBy;
             this._onTick();
-
-            this._clockSkew[this._skewPosition] = Math.abs(offBy);
-            this._skewPosition++;
-            if (this._clockSkew.length === this._skewPosition) {
-                const avg = this._clockSkew.reduce((s, c) => s + c, 0) / this._clockSkew.length;
-                console.log(`Avg Skew: ${avg}`);
-                this._skewPosition = 0;
-            }
         }
 
         const dueIn = this._nextDue - now;
         if (dueIn < 10) {
-            setImmediate(() => this.tick());
-        } else if (dueIn < 50) {
-            setTimeout(() => this.tick(), 10);
+            setImmediate(() => this.tick(tickNum));
         } else {
-            setTimeout(() => this.tick(), 30);
+            setTimeout(() => this.tick(tickNum), Math.floor(dueIn / 3));
         }
 
     }
