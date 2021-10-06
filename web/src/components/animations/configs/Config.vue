69b443a14cfefaa03815d2a93357b5294f0512ea
Josh Kraker <joshkrak@gmail.com>
2021-10-06 10:22:22 -0400

    Delete config

--- web/src/components/animations/configs/Config.vue
+++ web/src/components/animations/configs/Config.vue
@@ -1,13 +1,20 @@
 <template>
    <div class="h-100 d-flex flex-column">
-      <led-canvas
-         id="canvas"
-         :frame="frame"
-      ></led-canvas>
+      <led-canvas id="canvas" :frame="frame"></led-canvas>
 
       <div class="flex-grow-1 container shadow bg-white p-3 d-flex flex-column">
          <div class="flex-grow-1">
-            <h1>{{ animation.name }}</h1>
+            <div class="row">
+               <div class="col">
+                  <h1>{{ animation.name }}</h1>
+               </div>
+
+               <div class="col-auto d-flex align-items-center">
+                  <button class="btn text-danger" @click="deleteConfirmation = true">
+                     <i class="fal fa-trash-alt fa-lg"></i>
+                  </button>
+               </div>
+            </div>
 
             <div class="row">
                <div class="col-sm-6 col-lg-4 mb-3">
@@ -36,12 +43,8 @@
             </div>
 
             <div class="row">
-
                <div class="col-lg-6 col-xl mb-3 d-flex flex-column justify-content-end">
-                  <label
-                     for="c-interval"
-                     class="form-label"
-                  >
+                  <label for="c-interval" class="form-label">
                      Interval:
                      <input
                         class="interval"
@@ -100,9 +103,7 @@
                   <label
                      for="c-brightness"
                      class="form-label"
-                  >
-                     Brightness ({{Math.round(dirtyConfig.animation.brightness / 255 * 100)}}%)
-                  </label>
+                  >Brightness ({{ Math.round(dirtyConfig.animation.brightness / 255 * 100) }}%)</label>
                   <input
                      type="range"
                      class="form-range"
@@ -112,36 +113,24 @@
                      v-model.number="dirtyConfig.animation.brightness"
                   />
                </div>
-
             </div>
 
             <div class="row">
-               <div
-                  class="col-lg-6 mb-3"
-                  v-for="p of paramVms"
-                  :key="p.name"
-               >
-
-                  <div
-                     v-if="p.meta.type === 'number'"
-                     class="mb-3"
-                  >
+               <div class="col-lg-6 mb-3" v-for="p of paramVms" :key="p.name">
+                  <div v-if="p.meta.type === 'number'" class="mb-3">
                      <div class="row">
                         <div class="col-lg-auto">
-
                            <div class="form-floating">
                               <input
                                  :id="`config-${p.name}`"
                                  class="form-control"
                                  placeholder="*"
                                  v-model="p.value"
-                              >
-                              <label :for="`config-${p.name}`">{{p.name}}</label>
+                              />
+                              <label :for="`config-${p.name}`">{{ p.name }}</label>
                            </div>
-
                         </div>
                         <div class="col-lg d-flex align-items-end">
-
                            <input
                               type="range"
                               v-if="p.meta.type === 'number'"
@@ -153,32 +142,24 @@
                            />
                         </div>
                      </div>
-                     <small class="text-muted">
-                        {{ p.meta.description }}. Default {{ p.meta.default }}
-                     </small>
-
+                     <small
+                        class="text-muted"
+                     >{{ p.meta.description }}. Default {{ p.meta.default }}</small>
                   </div>
 
-                  <div
-                     class="mb-3"
-                     v-if="p.meta.type === 'color'"
-                  >
+                  <div class="mb-3" v-if="p.meta.type === 'color'">
                      <div class="row">
                         <div class="col-auto">
-                           <label>{{p.name}}</label>
+                           <label>{{ p.name }}</label>
                         </div>
                         <div class="col">
-                           <input
-                              type="color"
-                              v-model="p.value"
-                           >
-
-                           {{p.value}}
+                           <input type="color" v-model="p.value" />
+                           {{ p.value }}
                         </div>
                      </div>
-                     <small class="text-muted">
-                        {{ p.meta.description }}. Default {{ p.meta.default }}
-                     </small>
+                     <small
+                        class="text-muted"
+                     >{{ p.meta.description }}. Default {{ p.meta.default }}</small>
                   </div>
                </div>
             </div>
@@ -200,190 +181,208 @@
                </div>
 
                <div class="col-md-3 col-xxl-2 d-flex align-items-end mb-4">
-                  <button
-                     v-if="isDirty"
-                     class="btn w-100 btn-primary"
-                     @click="saveConfig()"
-                  >Save</button>
+                  <button v-if="isDirty" class="btn w-100 btn-primary" @click="saveConfig()">Save</button>
                </div>
             </div>
          </div>
       </div>
+
+      <v-confirmation-modal
+         v-if="deleteConfirmation"
+         @cancel="deleteConfirmation = false"
+         @confirm="deleteConfig()"
+      >Are you sure you want to delete this configuration?</v-confirmation-modal>
    </div>
 </template>
 
 <script lang="ts">
-   import { useAnimationRestClient, useDevicesRestClient, useThrottledProxy } from '@/services';
-   import { AnimationNamedConfigPost, ConfigMetaParam, deepClone, deepEquals, DeviceAnimationPost, DeviceAnimationResetPost, Frame, Id } from 'netled';
-   import { defineComponent, onUnmounted, reactive, watch, WatchStopHandle, ref, computed } from 'vue';
-   import LedCanvas from '@/components/LedCanvas.vue';
-   import { useIframeRunner } from '../editor/iframeRunner';
+import { useAnimationRestClient, useDevicesRestClient, useThrottledProxy } from '@/services';
+import { AnimationNamedConfigPost, ConfigMetaParam, deepClone, deepEquals, DeviceAnimationPost, DeviceAnimationResetPost, Frame, Id } from 'netled';
+import { defineComponent, onUnmounted, reactive, watch, WatchStopHandle, ref, computed } from 'vue';
+import LedCanvas from '@/components/LedCanvas.vue';
+import { useIframeRunner } from '../editor/iframeRunner';
+import { useRouter } from 'vue-router';
 
-   export default defineComponent({
-      components: {
-         LedCanvas
-      },
-      props: {
-         configId: { type: String as () => Id, required: true },
-      },
-      async setup(props) {
-         const stops: WatchStopHandle[] = [];
-         onUnmounted(() => stops.forEach((s) => s()));
+export default defineComponent({
+   components: {
+      LedCanvas
+   },
+   props: {
+      configId: { type: String as () => Id, required: true },
+   },
+   async setup(props) {
 
-         const animationClient = useAnimationRestClient();
-         const devicesClient = useDevicesRestClient();
+      const router = useRouter();
 
-         const devicesProm = devicesClient.list(true);
+      const stops: WatchStopHandle[] = [];
+      onUnmounted(() => stops.forEach((s) => s()));
 
-         //Not const because it's updated after a save
-         const config = ref(await animationClient.config.byId(props.configId));
+      const animationClient = useAnimationRestClient();
+      const devicesClient = useDevicesRestClient();
 
-         const animation = await animationClient.byId(
-            config.value.animation.id,
-            config.value.animation.version
-         );
+      const devicesProm = devicesClient.list(true);
 
-         const devices = await devicesProm;
+      //Not const because it's updated after a save
+      const config = ref(await animationClient.config.byId(props.configId));
 
-         const dirtyConfig = reactive(deepClone(config.value));
-         const description = computed({
-            get() { return dirtyConfig.description ?? ''; },
-            set(v: string) { dirtyConfig.description = v || null; }
-         });
+      const animation = await animationClient.byId(
+         config.value.animation.id,
+         config.value.animation.version
+      );
 
-         const iframe = await useIframeRunner(animation.script);
-         stops.push(iframe.dispose);
+      const devices = await devicesProm;
 
-         await iframe.setNumLeds(60);
+      const dirtyConfig = reactive(deepClone(config.value));
+      const description = computed({
+         get() { return dirtyConfig.description ?? ''; },
+         set(v: string) { dirtyConfig.description = v || null; }
+      });
 
-         const frame = ref<Frame>([]);
+      const iframe = await useIframeRunner(animation.script);
+      stops.push(iframe.dispose);
+
+      await iframe.setNumLeds(60);
+
+      const frame = ref<Frame>([]);
+      frame.value = await iframe.nextFrame();
+
+      const tick = async () => {
          frame.value = await iframe.nextFrame();
+      };
 
-         const tick = async () => {
-            frame.value = await iframe.nextFrame();
+      let lastInterval = config.value.animation.interval;
+      let intervalTimeout = setInterval(tick, config.value.animation.interval);
+
+      stops.push(() => clearInterval(intervalTimeout));
+
+      const configMeta = await iframe.getConfigMeta();
+
+      const paramVms = reactive(Object.getOwnPropertyNames(configMeta.params ?? {}).map(k => {
+         const vm: ParamVm = {
+            name: k,
+            meta: configMeta.params[k],
+            value: (config.value.animation.config ?? {})[k] ?? configMeta.params[k].default
          };
+         return vm;
+      }));
 
-         let lastInterval = config.value.animation.interval;
-         let intervalTimeout = setInterval(tick, config.value.animation.interval);
+      stops.push(watch(paramVms, vms => {
+         const newConfig = Object.fromEntries(vms.map(v => [v.name, v.value]));
+         (dirtyConfig.animation.config as Record<string, any>) = newConfig;
+         iframe.setConfig(newConfig);
+      }, { deep: true }));
 
-         stops.push(() => clearInterval(intervalTimeout));
-
-         const configMeta = await iframe.getConfigMeta();
-
-         const paramVms = reactive(Object.getOwnPropertyNames(configMeta.params ?? {}).map(k => {
-            const vm: ParamVm = {
-               name: k,
-               meta: configMeta.params[k],
-               value: (config.value.animation.config ?? {})[k] ?? configMeta.params[k].default
-            };
-            return vm;
-         }));
-
-         stops.push(watch(paramVms, vms => {
-            const newConfig = Object.fromEntries(vms.map(v => [v.name, v.value]));
-            (dirtyConfig.animation.config as Record<string, any>) = newConfig;
-            iframe.setConfig(newConfig);
-         }, { deep: true }));
-
-         const deviceVms = devices.filter(d => d.status.cameOnline > d.status.wentOffline).map(d => {
-            return reactive<DeviceVm>({
-               id: d.id,
-               name: d.name,
-               selected: false
-            });
+      const deviceVms = devices.filter(d => d.status.cameOnline > d.status.wentOffline).map(d => {
+         return reactive<DeviceVm>({
+            id: d.id,
+            name: d.name,
+            selected: false
          });
+      });
 
-         const toggleDevice = (d: DeviceVm) => {
-            d.selected = !d.selected;
-            if (d.selected) {
-               devicesClient.setAnimation({
-                  deviceIds: [d.id],
-                  animation: dirtyConfig.animation
-               });
-               devicesClient.stopAnimation({
-                  deviceIds: [d.id],
-                  stop: false,
-                  persist: false
-               });
-            } else {
-               devicesClient.resetAnimation({ deviceIds: [d.id] });
-            }
+      const toggleDevice = (d: DeviceVm) => {
+         d.selected = !d.selected;
+         if (d.selected) {
+            devicesClient.setAnimation({
+               deviceIds: [d.id],
+               animation: dirtyConfig.animation
+            });
+            devicesClient.stopAnimation({
+               deviceIds: [d.id],
+               stop: false,
+               persist: false
+            });
+         } else {
+            devicesClient.resetAnimation({ deviceIds: [d.id] });
+         }
+      };
+
+      const resetDevices = () => {
+         const previewDeviceIds = deviceVms.filter(d => d.selected).map(d => d.id);
+         if (!previewDeviceIds.length) { return; }
+         devicesClient.resetAnimation({ deviceIds: previewDeviceIds as DeviceAnimationResetPost['deviceIds'] });
+      };
+
+      stops.push(resetDevices);
+
+      const isDirty = computed(() => !deepEquals(config.value, dirtyConfig));
+
+      const saveConfig = async () => {
+         if (!isDirty.value) { return; }
+         const newConfig: AnimationNamedConfigPost = {
+            id: dirtyConfig.id,
+            name: dirtyConfig.name,
+            animation: dirtyConfig.animation,
+            description: dirtyConfig.description
          };
+         config.value = deepClone(dirtyConfig);
+         await animationClient.config.save(newConfig);
 
-         const resetDevices = () => {
-            const previewDeviceIds = deviceVms.filter(d => d.selected).map(d => d.id);
-            if (!previewDeviceIds.length) { return; }
-            devicesClient.resetAnimation({ deviceIds: previewDeviceIds as DeviceAnimationResetPost['deviceIds'] });
-         };
+      };
 
-         stops.push(resetDevices);
+      window.addEventListener('beforeunload', resetDevices, { capture: true });
+      stops.push(() => window.removeEventListener('beforeunload', resetDevices));
 
-         const isDirty = computed(() => !deepEquals(config.value, dirtyConfig));
+      const throttledSetAnimation = useThrottledProxy(devicesClient.setAnimation.bind(devicesClient));
 
-         const saveConfig = async () => {
-            if (!isDirty.value) { return; }
-            const newConfig: AnimationNamedConfigPost = {
-               id: dirtyConfig.id,
-               name: dirtyConfig.name,
-               animation: dirtyConfig.animation,
-               description: dirtyConfig.description
-            };
-            config.value = deepClone(dirtyConfig);
-            await animationClient.config.save(newConfig);
+      stops.push(watch(dirtyConfig, (c) => {
+         c.animation.interval = Math.round(c.animation.interval);
 
-         };
+         if (c.animation.interval !== lastInterval) {
+            clearInterval(intervalTimeout);
+            intervalTimeout = setInterval(tick, c.animation.interval);
+            lastInterval = c.animation.interval;
+         }
 
-         window.addEventListener('beforeunload', resetDevices, { capture: true });
-         stops.push(() => window.removeEventListener('beforeunload', resetDevices));
+         const previewDeviceIds = deviceVms.filter(d => d.selected).map(d => d.id);
+         if (previewDeviceIds.length) {
+            throttledSetAnimation({
+               deviceIds: previewDeviceIds as DeviceAnimationPost['deviceIds'],
+               animation: dirtyConfig.animation
+            });
+         }
+      }, { deep: true }));
 
-         const throttledSetAnimation = useThrottledProxy(devicesClient.setAnimation.bind(devicesClient));
+      const deleteConfirmation = ref(false);
 
-         stops.push(watch(dirtyConfig, (c) => {
-            c.animation.interval = Math.round(c.animation.interval);
-
-            if (c.animation.interval !== lastInterval) {
-               clearInterval(intervalTimeout);
-               intervalTimeout = setInterval(tick, c.animation.interval);
-               lastInterval = c.animation.interval;
+      const deleteConfig = async () => {
+         await animationClient.config.delete(props.configId);
+         router.replace({
+            name: 'animation-configs', params: {
+               animationId: config.value.animation.id,
+               version: config.value.animation.version
             }
+         });
+      };
 
-            const previewDeviceIds = deviceVms.filter(d => d.selected).map(d => d.id);
-            if (previewDeviceIds.length) {
-               throttledSetAnimation({
-                  deviceIds: previewDeviceIds as DeviceAnimationPost['deviceIds'],
-                  animation: dirtyConfig.animation
-               });
-            }
-         }, { deep: true }));
-
-         return { dirtyConfig, description, animation, frame, paramVms, deviceVms, toggleDevice, saveConfig, isDirty };
-      }
-
-   });
-
-   interface ParamVm {
-      name: string;
-      meta: ConfigMetaParam;
-      value: string | number;
+      return { dirtyConfig, description, animation, frame, paramVms, deviceVms, toggleDevice, saveConfig, isDirty, deleteConfirmation, deleteConfig };
    }
 
-   interface DeviceVm {
-      id: Id;
-      name: string;
-      selected: boolean;
-   }
+});
+
+interface ParamVm {
+   name: string;
+   meta: ConfigMetaParam;
+   value: string | number;
+}
+
+interface DeviceVm {
+   id: Id;
+   name: string;
+   selected: boolean;
+}
 </script>
 
 <style lang="postcss" scoped>
-   #canvas {
-      height: 20px;
-   }
+#canvas {
+   height: 20px;
+}
 
-   label > input {
-      width: 5rem;
-   }
+label > input {
+   width: 5rem;
+}
 
-   .btn-device {
-      max-width: 200px;
-   }
+.btn-device {
+   max-width: 200px;
+}
 </style>
