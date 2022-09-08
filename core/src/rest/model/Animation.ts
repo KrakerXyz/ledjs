6ae6c00105605c2c7f586377e21eb4763198ffe8
Josh Kraker <joshkrak@gmail.com>
2022-09-07 22:17:03 -0400

    WIP core changes

--- core/src/rest/model/Animation.ts
+++ core/src/rest/model/Animation.ts
@@ -4,13 +4,16 @@ export interface Animation {
     readonly id: Id;
     name: string;
     description?: string | null;
-    script: string;
+    /** Plain-old-javascript for the animation */
+    readonly js: string;
+    /** Typescript-based animation script */
+    ts: string;
     readonly published: boolean;
     readonly version: number;
     readonly created: number;
     readonly author: Id;
+    readonly $v: 2;
 }
 
-export type AnimationMeta = Omit<Animation, 'script'>;
-
-export type AnimationPost = Omit<Animation, 'version' | 'created' | 'author' | 'published'>;
+export type AnimationMeta = Omit<Animation, 'script' | 'js' | 'ts'>;
+export type AnimationPost = Omit<Animation, 'version' | 'created' | 'author' | 'published'| 'js'>;
